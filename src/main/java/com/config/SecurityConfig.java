c98fb963721566dac85bfd82453b800723570c6f
JkeyLove <1826067520@qq.com>
2023-04-22 01:15:53 +0800

    用户密码加密存储

---
+++ src/main/java/com/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    //创建BCryptPasswordEncoder注入容器
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/test/java/com/SangengApplicationTests.java
+++ src/test/java/com/SangengApplicationTests.java
@@ -5,6 +5,7 @@ import com.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -13,6 +14,25 @@ class SangengApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Test
+    public void TestBCryptPasswordEncoder(){
+        /*BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();*/
+        //$2a$10$JqmtKrKssXIsgM0glyaMAOXtOpNNsEs/7cCCLK.4ikNsy4DplB6vS是一个数据库的密文密码
+        System.out.println(passwordEncoder.matches("12345",
+                "$2a$10$JqmtKrKssXIsgM0glyaMAOXtOpNNsEs/7cCCLK.4ikNsy4DplB6vS"));
+
+        /*String encode = passwordEncoder.encode("1234");
+        String encode1 = passwordEncoder.encode("1234");
+        System.out.println(encode);
+        System.out.println(encode1);*/
+
+    }
+
 
     @Test
     public void testUserMapper(){
